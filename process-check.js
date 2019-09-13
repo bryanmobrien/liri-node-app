6d4b5edf0086a869f6c395a9699e8d24b1882921
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-09-13 00:48:51 -0500

    work on process-check.js

--- process-check.js
+++ process-check.js
@@ -1,1 +1,15 @@
-console.log(process.env);
+const envVars = [
+    'TERM',
+    'SHELL',
+    'USER',
+    'PATH',
+    'PWD',
+    'EDITOR',
+    'HOME',
+    'LOGNAME',
+ //   '_:'
+];
+for (let i = 0; i < envVars.length; i++ ) {
+    console.log(process.env.envVars[i]);
+    //console.log(process.env.envVars[i]);
+};
