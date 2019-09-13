88d63ba3c7141cdae7b61b697c11af5bc9410963
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-09-13 01:46:40 -0500

    more work on process-check.js

--- process-check.js
+++ process-check.js
@@ -1,15 +1,7 @@
-const envVars = [
-    'TERM',
-    'SHELL',
-    'USER',
-    'PATH',
-    'PWD',
-    'EDITOR',
-    'HOME',
-    'LOGNAME',
- //   '_:'
-];
-for (let i = 0; i < envVars.length; i++ ) {
-    console.log(process.env.envVars[i]);
-    //console.log(process.env.envVars[i]);
-};
+var objOfVals = process.env;
+
+// console.log(arrOfVals);
+// this consoles out the arrOfVals object as an array
+console.log(Object.entries(objOfVals));
+console.log(Object.length(objOfVals));
+var arrOfVals = [];
