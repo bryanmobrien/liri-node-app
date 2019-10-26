a99c9733cd4d25e238c50ac7ea1f7ae9330933ff
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-26 16:32:55 -0500

    correct jslinting errors

--- liri.js
+++ liri.js
@@ -40,8 +40,8 @@ for (var i = 3; i < nodeArgs.length; i++) {
 }
 
 var userCommand = process.argv[2];
-console.log(userCommand);
-console.log(process.argv);
+// console.log(userCommand);
+// console.log(process.argv);
 runLiri();
 
 //Switch statement for commands
@@ -53,11 +53,11 @@ function runLiri() {
             fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
                 if (error) {
                     console.log(error);
-                };
+                }
             });
 
             //Run request to bandsintown with the specified artist
-            var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
+            var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
             request(queryURL, function (error, response, body) {
                 //If no error and response is a success
                 if (!error && response.statusCode === 200) {
@@ -73,7 +73,7 @@ function runLiri() {
                         fs.appendFileSync("log.txt", "Venue: " + data[i].venue.name + "\n", function (error) {
                             if (error) {
                                 console.log(error);
-                            };
+                            }
                         });
 
                         //Get venue location
@@ -84,7 +84,7 @@ function runLiri() {
                             fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.country + "\n", function (error) {
                                 if (error) {
                                     console.log(error);
-                                };
+                                }
                             });
 
                         } else {
@@ -93,21 +93,21 @@ function runLiri() {
                             fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country + "\n", function (error) {
                                 if (error) {
                                     console.log(error);
-                                };
+                                }
                             });
                         }
 
                         //Get date of show
                         var date = data[i].datetime;
                         date = moment(date).format("MM/DD/YYYY");
-                        console.log("Date: " + date)
+                        console.log("Date: " + date);
                         //Append data to log.txt
                         fs.appendFileSync("log.txt", "Date: " + date + "\n----------------\n", function (error) {
                             if (error) {
                                 console.log(error);
-                            };
+                            }
                         });
-                        console.log("----------------")
+                        console.log("----------------");
                     }
                 }
             });
@@ -126,7 +126,7 @@ function runLiri() {
             fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
                 if (error) {
                     console.log(error);
-                };
+                }
             });
 
             console.log(spotify);
@@ -136,37 +136,37 @@ function runLiri() {
                 query: userInput
             }, function (err, data) {
                 if (err) {
-                    console.log("Error occured: " + err)
+                    console.log("Error occured: " + err);
                 }
 
                 //Assign data being used to a variable
-                var info = data.tracks.items
+                var info = data.tracks.items;
                 // console.log(info);
 
                 //Loop through all the "items" array
                 for (var i = 0; i < info.length; i++) {
                     //Store "album" object to variable
                     var albumObject = info[i].album;
-                    var trackName = info[i].name
-                    var preview = info[i].preview_url
+                    var trackName = info[i].name;
+                    var preview = info[i].preview_url;
                     //Store "artists" array to variable
-                    var artistsInfo = albumObject.artists
+                    var artistsInfo = albumObject.artists;
                     //Loop through "artists" array
                     for (var j = 0; j < artistsInfo.length; j++) {
-                        console.log("Artist: " + artistsInfo[j].name)
-                        console.log("Song Name: " + trackName)
-                        console.log("Preview of Song: " + preview)
-                        console.log("Album Name: " + albumObject.name)
-                        console.log("----------------")
+                        console.log("Artist: " + artistsInfo[j].name);
+                        console.log("Song Name: " + trackName);
+                        console.log("Preview of Song: " + preview);
+                        console.log("Album Name: " + albumObject.name);
+                        console.log("----------------");
                         //Append data to log.txt
                         fs.appendFileSync("log.txt", "Artist: " + artistsInfo[j].name + "\nSong Name: " + trackName + "\nPreview of Song: " + preview + "\nAlbum Name: " + albumObject.name + "\n----------------\n", function (error) {
                             if (error) {
                                 console.log(error);
-                            };
+                            }
                         });
                     }
                 }
-            })
+            });
 
             break;
         case "movie-this":
@@ -180,22 +180,22 @@ function runLiri() {
             fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
                 if (error) {
                     console.log(error);
-                };
+                }
             });
 
             //Run request to OMDB
-            var queryURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
+            var queryURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
             request(queryURL, function (error, response, body) {
                 if (!error && response.statusCode === 200) {
                     var info = JSON.parse(body);
-                    console.log("Title: " + info.Title)
-                    console.log("Release Year: " + info.Year)
-                    console.log("OMDB Rating: " + info.Ratings[0].Value)
-                    console.log("Rating: " + info.Ratings[1].Value)
-                    console.log("Country: " + info.Country)
-                    console.log("Language: " + info.Language)
-                    console.log("Plot: " + info.Plot)
-                    console.log("Actors: " + info.Actors)
+                    console.log("Title: " + info.Title);
+                    console.log("Release Year: " + info.Year);
+                    console.log("OMDB Rating: " + info.Ratings[0].Value);
+                    console.log("Rating: " + info.Ratings[1].Value);
+                    console.log("Country: " + info.Country);
+                    console.log("Language: " + info.Language);
+                    console.log("Plot: " + info.Plot);
+                    console.log("Actors: " + info.Actors);
 
                     //Append data to log.txt
                     fs.appendFileSync("log.txt", "Title: " + info.Title + "\nRelease Year: " + info.Year + "\nIMDB Rating: " + info.Ratings[0].Value + "\nRating: " +
@@ -203,7 +203,7 @@ function runLiri() {
                         function (error) {
                             if (error) {
                                 console.log(error);
-                            };
+                            }
                         });
                 }
             });
@@ -218,7 +218,7 @@ if (userCommand == "do-what-it-says") {
     //Read random.txt file
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
-            return console.log(error)
+            return console.log(error);
         }
 
         //Split data into array
@@ -227,5 +227,5 @@ if (userCommand == "do-what-it-says") {
         userInput = textArr[1];
         nextUserInput = userInput.replace(/%20/g, " ");
         runLiri();
-    })
+    });
 }
